9599b59ce69ecd0494793322b86409a4e36ac546
Solra Bizna <solra@bizna.name>
2009-03-08 08:41:31 +0000

    add celnet SCU

--- install_packages.sh
+++ install_packages.sh
@@ -4,4 +4,4 @@ set -e
 
 PREFIX=/usr/subcritical/lib/
 
-cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium $PREFIX
+cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium celnet/celnet.scu $PREFIX
