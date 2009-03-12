02ded244c9794725f87567a43896ff4eabd82469
Solra Bizna <solra@bizna.name>
2009-03-12 06:51:44 +0000

    Oops

--- install_packages.sh
+++ install_packages.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-PREFIX=$(sed -rne "s/^.*INSTALL_PATH=(.+)/\\1/p" < ~/.scbuild)
+PREFIX=$(sed -rne "s/^.*INSTALL_PATH=(.+)/\\1/p" < ~/.scbuild)/lib/
 
 cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium celnet/celnet.scu $PREFIX
