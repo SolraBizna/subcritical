e5727bd003d6ed055225e19b81a5e19bc0501991
solrabizna <>
2009-06-24 14:42:37 +0000

    update copyrights

--- sdl/sdlman.h
+++ sdl/sdlman.h
@@ -1,6 +1,6 @@
 /*
   This source file is part of the SubCritical core package set.
-  Copyright (C) 2008 Solra Bizna.
+  Copyright (C) 2008-2009 Solra Bizna.
 
   SubCritical is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as
