ed6eeba29088b2e185b35c9122b65189b8db38e7
Solra Bizna <solra@bizna.name>
2009-06-24 14:40:58 +0000

    install quantum and vectoracious now

--- install_packages.sh
+++ install_packages.sh
@@ -4,4 +4,4 @@ set -e
 
 PREFIX=$(sed -rne "s/^.*INSTALL_PATH=(.+)/\\1/p" < ~/.scbuild)/lib/
 
-cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium celnet/celnet.scu $PREFIX
+cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium celnet/celnet.scu vectoracious quantum/quantum.scu $PREFIX
