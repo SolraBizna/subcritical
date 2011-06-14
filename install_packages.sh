58961431d337e62fb44c1160d22bf540ebfa21a4
Solra Bizna <solra@bizna.name>
2011-06-14 19:52:08 +0000

    improve compatibility with non-GNU sed

--- install_packages.sh
+++ install_packages.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-PREFIX=$(sed -rne "s/^.*INSTALL_PATH=(.+)/\\1/p" < ~/.scbuild)/lib/
+PREFIX=$(grep ^INSTALL_PATH < ~/.scbuild | cut -b 14-)/lib/
 
 cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium celnet/celnet.scu vectoracious quantum/quantum.scu $PREFIX
