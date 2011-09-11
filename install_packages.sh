be7d84c5e14c5281be52af918674b81957f39a85
Solra Bizna <solra@bizna.name>
2011-09-11 10:07:22 +0000

    un-stupid, take 2

--- install_packages.sh
+++ install_packages.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-PREFIX=$(grep ^INSTALL_PATH < ~/.scbuild | cut -b 14-)/lib/
+PREFIX=$(grep ^INSTALL_PATH < ~/.scbuild | cut -b 14-)
 if test -z "$PREFIX"; then
-   PREFIX=$(grep ^REAL_INSTALL_PATH < ~/.scbuild | cut -b 19-)/lib/
+   PREFIX=$(grep ^REAL_INSTALL_PATH < ~/.scbuild | cut -b 19-)
 fi
 if test -z "$PREFIX"; then
     echo "No INSTALL_PATH or REAL_INSTALL_PATH is indicated in your ~/.scbuild"
     exit 1
 fi
 
-cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium celnet/celnet.scu vectoracious quantum/quantum.scu $PREFIX
+cp -r fontman/fontman.scu fontman/liberation garbage/garbage.scu libconfig/libconfig.scu scui/scui.scu scui/uranium celnet/celnet.scu vectoracious quantum/quantum.scu $PREFIX/lib/
