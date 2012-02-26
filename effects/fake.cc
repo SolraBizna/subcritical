11668eb445a01303be4d86a08533251aa2cab903
solrabizna <>
2012-02-26 21:38:52 +0000

    add FakeDrawable::Update convenience method

--- effects/fake.cc
+++ effects/fake.cc
@@ -28,9 +28,21 @@ public:
   PROTOCOL_PROTOTYPE();
   FakeDrawable(Drawable*restrict src, int x, int y, int w, int h);
   virtual ~FakeDrawable();
+  void Update(int x, int y, int w, int h) throw() {
+    if(!src->IsA("GraphicsDevice")) return;
+    GraphicsDevice*restrict devsrc = (GraphicsDevice*)src;
+    /* don't need to do any clipping here */
+    devsrc->Update(x+xoff,y+yoff,w,h);
+  }
+  void UpdateAll() throw() { Update(0, 0, width, height); }
+  int Lua_Update(lua_State* L) throw();
+private:
+  Drawable*restrict src;
+  int xoff, yoff;
 };
 
-FakeDrawable::FakeDrawable(Drawable*restrict src, int x, int y, int w, int h) {
+FakeDrawable::FakeDrawable(Drawable*restrict src, int x, int y, int w, int h) :
+  src(src), xoff(x), yoff(y) {
   this->width = w;
   this->height = h;
   this->has_alpha = src->has_alpha;
@@ -45,7 +57,11 @@ FakeDrawable::FakeDrawable(Drawable*restrict src, int x, int y, int w, int h) {
 
 FakeDrawable::~FakeDrawable() {}
 
-PROTOCOL_IMP_PLAIN(FakeDrawable, Drawable);
+static const struct ObjectMethod FDMethods[] = {
+  METHOD("Update", &FakeDrawable::Lua_Update),
+  NOMOREMETHODS(),
+};
+PROTOCOL_IMP(FakeDrawable, Drawable, FDMethods);
 
 #define COOKIE ((void*)(Construct_FakeDrawable))
 
@@ -81,4 +97,22 @@ SUBCRITICAL_CONSTRUCTOR(FakeDrawable)(lua_State* L) {
   lua_settable(L, -3);
   lua_pop(L, 1);
   return 1;
+}
+
+int FakeDrawable::Lua_Update(lua_State* L) throw() {
+  if(lua_gettop(L) == 0) UpdateAll();
+  else {
+    int x, y, w, h;
+    x = (int)luaL_checkinteger(L, 1);
+    y = (int)luaL_checkinteger(L, 2);
+    w = (int)luaL_checkinteger(L, 3);
+    h = (int)luaL_checkinteger(L, 4);
+    if(x < 0) { w += x; x = 0; }
+    if(x + w > width) w = width - x;
+    if(y < 0) { h += y; y = 0; }
+    if(y + h > height) h = height - y;
+    if(w <= 0 || h <= 0) return 0;
+    Update(x, y, w, h);
+  }
+  return 0;
 }
